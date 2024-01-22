9874dfc60271018beed1c65d58645dbec0de3a23
dewaynestaton <src.dewayne@gmail.com>
2024-01-21 23:05:28 -0800

    Complete module 2 MVC setup

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+// Built-in Node JS file system component
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
 // GET travel view
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
